ecd7cb537f1fe90369f5e58c241eeaccdd890b29
Pablo Valdesoiro Pulido <pvaldesoiro@digitalorigin.com>
2018-08-16 20:30:16 +0200

    First commit

---
+++ src/main/java/Bottles.java
@@ -0,0 +1,13 @@
+public class Bottles {
+    public String verse(int i) {
+        return null;
+    }
+
+    public String verses(int i, int j) {
+        return null;
+    }
+
+    public String song() {
+        return null;
+    }
+}
