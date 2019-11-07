d1a99badbf57a18983963ebccb6de206ade8fb95
Pablo Valdesoiro Pulido <pvaldesoiro@digitalorigin.com>
2019-11-07 10:50:47 +0100

    Fix test typo

--- src/test/java/BottlesShould.java
+++ src/test/java/BottlesShould.java
@@ -18,8 +18,8 @@ public class BottlesShould {
     @Disabled
     public void
     return_another_verse() {
-        var expected = "89 bottles of beer on the wall, 99 bottles of beer.\n" +
-                "Take one down and pass it around, 89 bottles of beer on the wall.\n";
+        var expected = "89 bottles of beer on the wall, 89 bottles of beer.\n" +
+                "Take one down and pass it around, 88 bottles of beer on the wall.\n";
 
         assertEquals(expected, new Bottles().verse(89));
     }
